53820203da140cd5860d5b8e1a76e367d8bc62a1
jolandawester123 <jolandawesterjennys9j@hotmail.com>
2024-09-19 15:44:30 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("dbb4a72ad40c12409d841878ef9f1ad42f939128ea80c60f2cf61dbe6949cac6", "dbb4a72ad40c12409d841878ef9f1ad42f939128ea80c60f2cf61dbe6949cac6") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
